bd802f9a9a62d4f84393498a4a522738dc02a515
Golfredo Perez Fernandez <golfredo.pf@gmail.com>
2023-05-08 16:10:15 -0400

    asd

--- src/components/slider/slider-item/index.jsx
+++ src/components/slider/slider-item/index.jsx
@@ -26,8 +26,6 @@ function SliderItem(props) {
 		  mouseControls: true,
 		  touchControls: true,
 		  gyroControls: false,
-		  minHeight: 200.00,
-		  minWidth: 200.00,
 		  scale: 1.00,
 		  scaleMobile: 1.00,
 		  color: 0xdeb410,
